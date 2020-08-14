8eae0c086058039f7ebf25345ec224091ca1e993
Aleuci Lima <aleuci.lima@gmail.com>
2020-08-14 08:43:10 -0300

    Update mediaAlunos.js

--- Modulo 1 - JavaScript/mediaAlunos.js
+++ Modulo 1 - JavaScript/mediaAlunos.js
@@ -1,8 +1,8 @@
 //Criar um programa que calcula a média
-//das notas entre os alunos e envia
+//das turmas de alunos e envia
 //mensagem do cálculo da média
 
-const alunos = [
+const alunosDaTurmaA = [
     {
         nome: 'Mayk',
         nota: 9.8
@@ -17,10 +17,35 @@ const alunos = [
     }
 ]
 
-const media = (alunos[0].nota + alunos[1].nota + alunos[2].nota) / 3
+const alunosDaTurmaB = [
+    {
+        nome: 'Fulano',
+        nota: 9.8
+    },
+    {
+        nome: 'Ciclano',
+        nota: 6
+    },
+    {
+        nome: 'Beltrano',
+        nota: 2
+    }
+]
 
-if (media > 5) {
-    console.log(`A média foi de ${media}. Parabéns!`)
-} else {
-    console.log(`A média foi de ${media}. Reprovado!`)
+function calculaMedia(alunos) {
+    return (alunos[0].nota + alunos[1].nota + alunos[2].nota) / 3
 }
+
+const media1 = calculaMedia(alunosDaTurmaA)
+const media2 = calculaMedia(alunosDaTurmaB)
+
+function enviaMensagem(media, turma) {
+    if (media > 5) {
+        console.log(`A média da turma ${turma} foi de ${media}. Parabéns!`)
+    } else {
+        console.log(`A média da ${turma} foi menor que 5!`)
+    }
+}
+
+enviaMensagem(media1, 'Turma A')
+enviaMensagem(media2, 'Turma B')
