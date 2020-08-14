da264f6fc743af0b4e16af9c81cf9273ef75a517
Aleuci Lima <aleuci.lima@gmail.com>
2020-08-14 15:36:27 -0300

    Prévia novo desafio

---
+++ Modulo 1 - JavaScript/desafio1-3-1.js
@@ -0,0 +1,5 @@
+//Crie um programa que armazena um array de usuários
+//(objetos), cada usuário tem um nome e suas tecnologias(novo array).
+//Percorra a lista de usuários com uma estrutura de repetição imprimindo em tela
+//informações dos usuários.
+
