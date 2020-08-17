da71b0345f95d0c04251a8de79e5957c414cb28c
Aleuci Lima <aleuci.lima@gmail.com>
2020-08-17 11:55:52 -0300

    Checa usuários

--- Modulo 1 - JavaScript/desafio1-3-1.js
+++ Modulo 1 - JavaScript/desafio1-3-1.js
@@ -4,11 +4,24 @@
 //informações dos usuários.
 
 const usuarios = [
-    { nome: "Carlos", tecnologias: ["HTML", " CSS"] },
-    { nome: "Jasmine", tecnologias: ["JavaScript", " CSS"] },
+    { nome: "Carlos", tecnologias: ["HTML", "CSS"] },
+    { nome: "Jasmine", tecnologias: ["JavaScript", "CSS"] },
     { nome: "Tuane", tecnologias: ["HTML", " Node.js"] }
   ];
 
-for(let i = 0; i < usuarios.length; i++) {
-    console.log(`${usuarios[i].nome} trabalha com ${usuarios[i].tecnologias}`)
+function checaUsuario(usuario) {
+  for(let i = 0; i < usuario.tecnologias.length ; i++) {
+    if(usuario.tecnologias[i] === 'CSS'){
+        return true
+    }
+  }
+  return false
 }
+
+for(let i = 0; i < usuarios.length; i++) {
+  if(checaUsuario(usuarios[i])){
+    console.log(`O usuário ${usuarios[i].nome} trabalha com CSS`)
+  }
+}
+
+
