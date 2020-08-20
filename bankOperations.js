a3c82da8e41ced05592957338f94035db0474b84
Aleuci Lima <aleuci.lima@gmail.com>
2020-08-20 16:22:39 -0300

    Bank Operations

---
+++ bankOperations.js
@@ -0,0 +1,22 @@
+//Crie um programa para realizar operações bancárias 
+//na conta de um usuário.
+
+const user = {
+    name: "Mariana",
+    transactions: [],
+    balance: 0
+}
+
+createTransaction({ type: "credit", value: 50 });
+createTransaction({ type: "credit", value: 120 });
+createTransaction({ type: "debit", value: 80 });
+createTransaction({ type: "debit", value: 30 });
+
+console.log(user.balance); // 60
+
+getHigherTransactionByType("credit"); // { type: 'credit', value: 120 }
+getHigherTransactionByType("debit"); // { type: 'debit', value: 80 }
+
+getAverageTransactionValue(); // 70
+
+getTransactionsCount(); // { credit: 2, debit: 2 }
